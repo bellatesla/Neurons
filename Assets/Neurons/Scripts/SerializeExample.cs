cee64cb7e8820b43849e3e862c2d7830fa64fc3f
bryan.meadows <bryan.meadows@astrazeneca.com>
2024-08-14 21:39:51 -0700

    added serialization example

--- Assets/Neurons/Scripts/SerializeExample.cs
+++ Assets/Neurons/Scripts/SerializeExample.cs
@@ -1,38 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using JsonSubTypes;
 using UnityEngine;
 
 public class SerializeExample : MonoBehaviour
 {
     public Container container = new Container();
-
+    public GameObject testObject;
+    public Transform testTransform;
     void Start()
     {
         // Adding derived class instances to the list
-        container.items.Add(new NeuronSaveData { commonProperty = "Base A", specificPropertyA = "Specific Property A" });
-        container.items.Add(new UITextdata { commonProperty = "Base B", specificPropertyB = 5.5f });
+        container.items.Add(new DerivedClassA { commonProperty = "Base A", transformPos = testTransform.position }) ;
+        container.items.Add(new DerivedClassB { commonProperty = "Base B", specificPropertyB = testObject.name });
 
         // Serialize the container with ReferenceLoopHandling.Ignore
         string json = JsonConvert.SerializeObject(container, Formatting.Indented,
-            new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
-        Debug.Log("Serialized JSON: " + json);
+           new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore ,});
+        //string jsonData = JsonUtility.ToJson(saveData, true);//does not handle derived data
+        string path = Application.persistentDataPath + "/" + "Serialize Test.json";
+        System.IO.File.WriteAllText(path, json);
+        Debug.Log("Saved to path: " + path);
 
-        //// Deserialize it back
-        Container deserializedContainer = JsonConvert.DeserializeObject<Container>(json);
+        // Deserialize it back
+        
+        string jsonData = System.IO.File.ReadAllText(Application.persistentDataPath + "/" + "Serialize Test.json");
+        //DataContainer saveData = JsonConvert.DeserializeObject<DataContainer>(jsonData);
+        Container deserializedContainer = JsonConvert.DeserializeObject<Container>(jsonData);
 
         foreach (var item in deserializedContainer.items)
         {
             Debug.Log("Common Property: " + item.commonProperty);
 
-            if (item is NeuronSaveData derivedA)
+            if (item is DerivedClassA derivedA)
                 Debug.Log("Specific Property A: " + derivedA.specificPropertyA);
-            else if (item is UITextdata derivedB)
+            else if (item is DerivedClassB derivedB)
                 Debug.Log("Specific Property B: " + derivedB.specificPropertyB);
         }
     }
 }
 public class Container
 {
-    public List<SaveableTypeBase> items = new List<SaveableTypeBase>();
+    public List<BaseClass> items = new List<BaseClass>();
 }
+
+[JsonConverter(typeof(JsonSubtypes), "Type")]
+[JsonSubtypes.KnownSubType(typeof(DerivedClassA), "A")]
+[JsonSubtypes.KnownSubType(typeof(DerivedClassB), "B")]
+public class BaseClass
+{
+    public string commonProperty;
+    public string Type;
+    public Vector3 position;  // Example of a Unity type
+
+}
+
+public class DerivedClassA : BaseClass
+{
+    public string specificPropertyA;
+    public Vector3 transformPos;
+    public DerivedClassA()
+    {
+        Type = "A";
+    }
+}
+
+public class DerivedClassB : BaseClass
+{
+    public string specificPropertyB;
+    // not working public GameObject gameObject;
+    public DerivedClassB()
+    {
+        Type = "B";
+    }
+}
+
